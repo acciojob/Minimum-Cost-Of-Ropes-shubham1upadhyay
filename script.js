9141b0782d35d06ec35918f365a18d2f63b8d136
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-16 17:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+      const input = document.getElementById("ropesInput").value;
+      const ropes = input.split(",").map(Number);
+
+      let totalCost = 0;
+
+      while (ropes.length > 1) {
+        ropes.sort((a, b) => a - b);
+        const cost = ropes[0] + ropes[1];
+        totalCost += cost;
+
+        ropes.splice(0, 2);
+        ropes.push(cost);
+      }
+
+      document.getElementById("result").innerHTML = totalCost;
+    }
